f33a28479727975979ea8ae347a7b1370ebd2b64
elbydata <info@elbydata.com>
2020-09-30 20:13:18 +0100

    v2 build dist

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyfer",
-    version="2.0.0",
+    version="2.0.1",
     author="Elby Data",
     author_email="info@elbydata.com",
     description="Simple string encryption",
